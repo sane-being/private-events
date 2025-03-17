fa7fb08cf9ad2b2b2e941818660963ecbfee9ca2
shrey <117995509+sane-being@users.noreply.github.com>
2025-03-17 12:14:31 +0000

    show past and upcoming events on Event#index
    Separate the past and upcoming events on the Event’s Index page
    by creating two class methods on the Event model (e.g. Event.past).
    
    - create two class methods #past & #upcoming
    in app/models/event.rb
    that give past & upcoming events respectively, using Event.where
    
    - show past and upcoming events separately on
    app/views/events/index.html.erb

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendees, through: :attendance, source: :attended_events
   has_many :attendances, foreign_key: :attended_event_id
+
+  def self.past
+    Event.where("date < ?", Date.today)
+  end
+
+  def self.upcoming
+    Event.where("date >= ?", Date.today)
+  end
 end
