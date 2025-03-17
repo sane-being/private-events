90390d3723e9f3c118ce9918f26a22f9fdb0ce3f
shrey <117995509+sane-being@users.noreply.github.com>
2025-03-17 18:18:37 +0000

    fix: add association dependency for deleting
    e.g. Use case: deleting event deletes all attendances associated with it
    - added 'dependent: :destroy' to all 'has_many' associations
    (not having through)

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
 
   # associations
   belongs_to :creator, class_name: "User"
-  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :attendances
 
   # scopes
